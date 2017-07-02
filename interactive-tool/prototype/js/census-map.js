06b69dfdf0e62884700245b60d114862975d3530
bwputman <bwputman@berkeley.edu>
2017-07-02 19:43:35 -0400

    add expanding control widget

--- interactive-tool/prototype/js/census-map.js
+++ interactive-tool/prototype/js/census-map.js
@@ -1,25 +1,55 @@
-// Create map
-var map = new L.Map('map', {
-	center: [37.770, -122.41],
-	zoom: 6
-})
+var body;
+var map_container;
+var census_map;
+var control_widget;
+var widget_expander;
 
-// Remove leaflet attribution from the map (we'll put attributions somewhere else)
-document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
+function initializeMap() {
+  // Create map object
+  var map = new L.Map('map', {
+  	center: [37.770, -122.41],
+  	zoom: 6
+  });
+  
+  // Remove leaflet attribution from the map (we'll put attributions somewhere else)
+  document.getElementsByClassName('leaflet-control-attribution')[0].style.display = 'none';
+  
+  // Add base layer to map
+  // TODO: can/should the base layer be stored locally?
+  base_layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+  base_layer.addTo(map);
+  
+  // Add census layer to map
+  census_layer = new L.Shapefile('data/ca-census-tract-shapefiles.zip', {
+    style: function(feature) {
+      return {color: 'grey',
+              opacity: 0.1,
+              weight: 1,
+              fillColor: 'blue', 
+              fillOpacity: (0.1 + 0.35 * (feature.properties.TRACTCE % 3))};
+    }
+  });
+  census_layer.addTo(map);
+  
+  // TODO: create d3 handles for control widget objects
+  body = d3.select('body');
+  map_container = body.select('#map_container');
+  census_map = map_container.select('#map');
+  control_widget = map_container.select('#control_widget');
+  widget_expander = map_container.select('input');
 
-// Add base layer
-// TODO: can/should the base layer be stored locally?
-base_layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-base_layer.addTo(map);
+}
 
-// Add census layer
-census_layer = new L.Shapefile('data/ca-census-tract-shapefiles.zip', {
-  style: function(feature) {
-    return {color: 'grey',
-            opacity: 0.1,
-            weight: 1,
-            fillColor: 'blue', 
-            fillOpacity: (0.1 + 0.35 * (feature.properties.TRACTCE % 3))};
-  }
-});
-census_layer.addTo(map);
+function openControlWidget() {
+  control_widget.style('width', '40%')
+                .style('left', '60%');
+  widget_expander.attr('value', '>>')
+                 .attr('onclick', 'closeControlWidget()');
+}
+
+function closeControlWidget(){
+  control_widget.style('width', '2%')
+                .style('left', '98%');
+  widget_expander.attr('value', '<<')
+                 .attr('onclick', 'openControlWidget()');
+}
