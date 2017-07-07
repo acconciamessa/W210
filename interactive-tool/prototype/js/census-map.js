5eb7479c03d385af50a89b7f99c31a379353442e
bwputman <bwputman@berkeley.edu>
2017-07-07 12:39:36 -0400

    patch to save feture settings when control widget is minimized

--- interactive-tool/prototype/js/census-map.js
+++ interactive-tool/prototype/js/census-map.js
@@ -40,6 +40,12 @@ function initializeMap() {
                                   .text('<<');
   
   num_features = 4;
+  possible_operators = ['+', '-', 'x'];
+  possible_units = [['%', '$', '\u00B0F', 'lb'],
+                    ['%', '$', '\u00B0F', 'lb'],
+                    ['%', '$', '\u00B0F', 'lb'],
+                    ['%', '$', '\u00B0F', 'lb']];
+  
   feature_handles = [null, null, null, null];
   feature_names = ['Feature Number 1', 'Feature Number 2', 'Feature Number 3', 'Feature Number 4'];
   feature_operators = ['+', '+', '+', '+'];
@@ -69,23 +75,36 @@ function openControlWidget() {
   // Loop over feature arrays to create individual feature controls        
   for (var i = 0; i < num_features; i++) {
     feature_handles[i] = control_widget.append("form")
-                                       .attr('id', 'feature_control')
+                                       .attr('class', 'feature_control')
+                                       .attr('name', 'feature_' + String(i))
                                        .style('top', String(50 + (40*i)));
                                        
     // Add feature name     
     feature_handles[i].append("text")
-                      .attr('id', 'feature_name')
+                      .attr('class', 'feature_name')
                       .text(feature_names[i]);
                       
     // Add feature operator dropdown         
     feature_handles[i].append("select")
                       .attr('title', 'Select Operator')
                       .attr('id', 'operator');
-    feature_handles[i].select("#operator").append('option').attr('id', '+').text('+'); 
-    feature_handles[i].select("#operator").append('option').attr('id', 'x').text('x');           
-    feature_handles[i].select("#operator").append('option').attr('id', '-').text('-');
-    // TODO: Default selection to feature_operators[i]
-        
+    
+    // Add operator selection options (default selection == feature_operators[i])
+    // TODO: Find a cleaner way to do this
+    for (j = 0; j < possible_operators.length; j++) {
+      if (possible_operators[j] == feature_operators[i]) {
+        feature_handles[i].select("#operator").append("option")
+                                              .attr('name', possible_operators[j])
+                                              .attr('selected', 'selected')
+                                              .text(possible_operators[j]);
+      }
+      else {
+        feature_handles[i].select("#operator").append("option")
+                                              .attr('name', possible_operators[j])
+                                              .text(possible_operators[j]);
+      }
+    }
+    
     // Add feature value input field          
     feature_handles[i].append("input")
                       .attr('title', 'Enter Value')
@@ -98,18 +117,31 @@ function openControlWidget() {
                       .attr('title', 'Select Unit')
                       .attr('value', String(feature_units[i]))
                       .attr('id', 'unit');
-    feature_handles[i].select("#unit").append('option').text('%'); 
-    feature_handles[i].select("#unit").append('option').text('$');
-    feature_handles[i].select("#unit").append('option').text('\u00B0F');
-    feature_handles[i].select("#unit").append('option').text('lb.');
-    // TODO: Default selection to feature_units[i]
+                      
+    // Add unit selection options (default selection == feature_units[i])
+    // TODO: Find a cleaner way to do this
+    for (j = 0; j < possible_units[i].length; j++) {
+      if (possible_units[i][j] == feature_units[i]) {
+        feature_handles[i].select("#unit").append("option")
+                                            .attr('name', possible_units[i][j])
+                                            .attr('selected', 'selected')
+                                            .text(possible_units[i][j]);
+      }
+      else {
+        feature_handles[i].select("#unit").append("option")
+                                            .attr('name', possible_units[i][j])
+                                            .text(possible_units[i][j]);
+      }
+    }
       
     // Add feature reset button                
     feature_handles[i].append("input")
-                      .attr('type', 'reset')
+                      .attr('type', 'button')
                       .attr('title', "Reset " + String(feature_names[i]))
-                      .attr('id', 'reset')
-                      .attr('value', '\u21BA');
+                      .attr('id', 'reset_button')
+                      .attr('name', 'reset_' + String(i))
+                      .attr('value', '\u21BA')
+                      .on('click', function() {resetFeature(parseInt(this.name.slice(-1)));});
                       
   } // End of feature creation loop
 
@@ -184,6 +216,8 @@ function openControlWidget() {
 } // End of openControlWidget() function
 
 function closeControlWidget(){
+  // Gather user-defined information
+  saveFeatureInput();
   
   // Clear all control widget children from the DOM and shrink the widget
   control_widget.selectAll('*').remove();
@@ -214,11 +248,7 @@ function addYear(){
 
 function recolorMap(){
   // Gather user-defined information
-  for (var i = 0; i < num_features; i++) {
-    feature_operators[i] = feature_handles[i].select("#operator").node().value;
-    feature_values[i] = feature_handles[i].select("#value").node().value;
-    feature_units[i] = feature_handles[i].select("#unit").node().value;
-  }
+  saveFeatureInput();
   
   // TODO: Use the user-defined information to recolor map
   var output = "Function \"recolorMap()\" has not been implemented yet.\n\n";
@@ -233,3 +263,46 @@ function recolorMap(){
    
    alert(output);
 } // End of recolorMap() function
+
+
+function saveFeatureInput() {
+   for (var i = 0; i < num_features; i++) {
+    feature_operators[i] = feature_handles[i].select("#operator").node().value;
+    feature_values[i] = feature_handles[i].select("#value").node().value;
+    feature_units[i] = feature_handles[i].select("#unit").node().value;
+  }
+}
+
+
+function resetFeature(reset_id) {
+  // Revert current operator to default
+  feature_operators[reset_id] = '+';
+  var operators = feature_handles[reset_id].select("#operator").node().options;
+  for (var i = 0; i < operators.length; i++) {
+    if (operators[i].getAttribute('name') == feature_operators[reset_id]) {
+      operators[i].setAttribute('selected', 'selected');
+    }
+    else {
+      operators[i].removeAttribute('selected');
+    }
+  }
+  
+  // Revert current value to default
+  feature_values[reset_id] = 0;
+  feature_handles[reset_id].select('#value').attr('value', feature_values[0]);
+  
+  // Revert current unit to default
+  feature_units[reset_id] = '%';
+  var units = feature_handles[reset_id].select("#unit").node().options;
+  for (i = 0; i < units.length; i++) {
+    if (units[i].getAttribute('name') == feature_units[reset_id]) {
+      units[i].setAttribute('selected', 'selected');
+    }
+    else {
+      units[i].removeAttribute('selected');
+    }
+  }
+  
+  // Reset form with current operator/value/unit
+  feature_handles[reset_id].node().reset();
+}
